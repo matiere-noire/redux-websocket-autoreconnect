a4390a037b086cb8c6b37ce7acddf3a1d2c8d71d
Kevin Maes <kevin@tek212.com>
2016-11-22 11:22:06 -0500

    compile test before running

--- test/test.js
+++ test/test.js
@@ -1,15 +1,21 @@
 /* eslint-env mocha */
-const td = require('testdouble');
-const chai = require('chai');
+// import deepFreeze from 'deep-freeze';
+import td from 'testdouble';
+import chai from 'chai';
 
 const { expect, assert } = chai;
 
-const middleware = require('../dist/').default;
+// Export an object with spec helper functions or libraries.
+// export { describe, it, beforeEach, afterEach, deepFreeze, td, expect, assert };
+
+// import { describe, it, td } from '../../spec_helper';
+import middleware from '../src/';
 
 describe('middleware', () => {
   it('should be a curried function that calls next(action)', () => {
     const action = {};
     const next = td.func('next');
+
     middleware()(next)(action);
 
     td.verify(next(action));
