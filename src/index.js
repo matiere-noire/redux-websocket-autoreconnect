8d40d16d919621e0a783ce937f048d07635fc595
Kevin Maes <kevin@tek212.com>
2016-12-09 20:03:13 -0500

    make more functional

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 /* eslint-env browser */
 /* @flow */
+import { compose } from 'redux';
 import { connecting, open, closed, message } from './actions';
 import { createWebsocket } from './websocket';
 
@@ -18,22 +19,17 @@ const createMiddleware = () => {
   // Hold a reference to the WebSocket instance in use.
   let websocket: ?WebSocket;
 
-  const onOpen = store => (event: Event) => store.dispatch(open(event));
-  const onClose = store => (event: Event) => store.dispatch(closed(event));
-  const onMessage = store => (event: MessageEvent) => store.dispatch(message(event));
-  const onConnecting = store => (event: Event) => store.dispatch(connecting(event, websocket));
-
   /**
    * A function to create the WebSocket object and attach the standard callbacks
    */
-  const initialize = (store, config: Config) => {
+  const initialize = ({ dispatch }, config: Config) => {
     websocket = createWebsocket(config);
 
-    websocket.onopen = onOpen(store);
-    websocket.onclose = onClose(store);
-    websocket.onmessage = onMessage(store);
+    websocket.onopen = compose(dispatch, open)
+    websocket.onclose = compose(dispatch, closed);
+    websocket.onmessage = compose(dispatch, message);
     // An optimistic callback assignment for WebSocket objects that support this
-    websocket.onconnecting = onConnecting(store);
+    websocket.onconnecting = compose(dispatch, connecting);
   };
 
   /**
