5694fe8ef8f2e650d78e90af190155e32e4df6a9
David Michael <david.michael@giantmachines.com>
2016-12-11 09:29:41 -0500

    working to test the index

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,6 +1,10 @@
 /* eslint-env mocha */
+import expect from 'expect';
+import configureMockStore from 'redux-mock-store';
 import td from 'testdouble';
-import middleware from '../src/';
+import middleware, { WEBSOCKET_CONNECT } from '../src/';
+
+const mockStore = configureMockStore([middleware]);
 
 describe('middleware', () => {
   it('should be a curried function that calls next(action)', () => {
@@ -10,5 +14,22 @@ describe('middleware', () => {
     middleware()(next)(action);
 
     td.verify(next(action));
+  });
+
+  describe('WEBSOCKET_CONNECT', () => {
+    const action = { type: WEBSOCKET_CONNECT };
+    const next = td.func('next');
+    class WebSocketStub {}
+    global.WebSocket = WebSocketStub;
+
+    it('call next action', () => {
+      middleware(mockStore)(next)(action);
+      td.verify(next(action));
+    });
+
+    it('', () => {
+      middleware(mockStore)(next)(action);
+      expect(middleware.websocket);
+    });
   });
 });
