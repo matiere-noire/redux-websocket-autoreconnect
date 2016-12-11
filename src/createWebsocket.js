432bf7eb2fed20510f6c74353b60b22acab0535c
Kevin Maes <kevin@tek212.com>
2016-12-10 22:33:16 -0500

    add some docs

--- src/createWebsocket.js
+++ src/createWebsocket.js
@@ -4,6 +4,8 @@
  * Converts an object containing argument information, used to instantiate
  * a websocket, into an array of args, according to priority.
  *
+ * @param {object} payload - The payload containing constructor and arg data.
+ *
  * @return {*[]} The array of arguments to be passed to a constructor function.
  */
  export const extractArgs = ({ args = [], url = '' } = {}) => {
@@ -15,6 +17,11 @@
 
  /**
   * Assemble arguments and instantiate a new websocket.
+  * @param {object} payload - The payload containing constructor and arg data.
+  * @param {function} getArgs - A getter funcion for extracting arguments from
+  * the payload.
+  *
+  * @return [*|WebSocket] An instance of WebSocket or a similar object.
   */
 export default (payload, getArgs) => {
   return new payload.constructor(...getArgs(payload));
