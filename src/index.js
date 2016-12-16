24c4e91ab843c57f9ccec46a561b270056723a22
David Michael <david.michael@giantmachines.com>
2016-12-16 14:41:10 -0500

    some cleanup and version bump

--- src/index.js
+++ src/index.js
@@ -13,7 +13,6 @@ export const WEBSOCKET_SEND = 'WEBSOCKET:SEND';
 // Action types dispatched by the WebSocket implementation
 export const WEBSOCKET_CONNECTING = 'WEBSOCKET:CONNECTING';
 export const WEBSOCKET_OPEN = 'WEBSOCKET:OPEN';
-export const WEBSOCKET_DISCONNECTING = 'WEBSOCKET:DISCONNECTING';
 export const WEBSOCKET_CLOSED = 'WEBSOCKET:CLOSED';
 export const WEBSOCKET_MESSAGE = 'WEBSOCKET:MESSAGE';
 
